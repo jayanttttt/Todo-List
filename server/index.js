ea0f0c899b8d45e57e2564b04552bd79f0ccf892
Jayant <yash22350@gmail.com>
2024-01-08 16:28:55 +0530

    Fixed cors error

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 
 connectDb();
